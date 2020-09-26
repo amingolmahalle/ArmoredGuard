b1687a7f648a5b9491ad98dc7210a1e76dd804e0
amingolmahalle <a.golmahalleh@alibaba.ir>
2020-09-26 23:15:53 +0330

    First Commit

---
+++ Common/Helpers/ModelBuilderExtensions.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Common.Helpers
+{
+    public static class ModelBuilderExtensions
+    {
+        /// <summary>
+        /// Dynamicaly load all IEntityTypeConfiguration with Reflection
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        /// <param name="assemblies">Assemblies contains Entities</param>
+        public static void RegisterEntityTypeConfiguration(this ModelBuilder modelBuilder, params Assembly[] assemblies)
+        {
+            MethodInfo applyGenericMethod = typeof(ModelBuilder).GetMethods()
+                .First(m => m.Name == nameof(ModelBuilder.ApplyConfiguration));
+
+            IEnumerable<Type> types = assemblies.SelectMany(a => a.GetExportedTypes())
+                .Where(c => c.IsClass && !c.IsAbstract && c.IsPublic);
+
+            foreach (Type type in types)
+            {
+                foreach (Type iface in type.GetInterfaces())
+                {
+                    if (iface.IsConstructedGenericType &&
+                        iface.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>))
+                    {
+                        MethodInfo applyConcreteMethod =
+                            applyGenericMethod.MakeGenericMethod(iface.GenericTypeArguments[0]);
+                        applyConcreteMethod.Invoke(modelBuilder, new object[] {Activator.CreateInstance(type)});
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Dynamicaly register all Entities that inherit from specific BaseType
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        /// <param name="baseType">Base type that Entities inherit from this</param>
+        /// <param name="assemblies">Assemblies contains Entities</param>
+        public static void RegisterAllEntities<TBaseType>(this ModelBuilder modelBuilder, params Assembly[] assemblies)
+        {
+            IEnumerable<Type> types = assemblies.SelectMany(a => a.GetExportedTypes())
+                .Where(c => c.IsClass && !c.IsAbstract && c.IsPublic && typeof(TBaseType).IsAssignableFrom(c));
+
+            foreach (Type type in types)
+                modelBuilder.Entity(type);
+        }
+    }
+}
---
+++ WebFramework/Configurations/ServiceCollectionExtensions.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Common.Helpers;
+using Data.Contracts;
+using Entities.User;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebFramework.Configurations
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddJwtAuthentication(this IServiceCollection services, JwtSettings jwtSettings)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                var secretKey = Encoding.UTF8.GetBytes(jwtSettings.SecretKey);
+                var encryptionKey = Encoding.UTF8.GetBytes(jwtSettings.EncryptKey);
+
+                var validationParameters = new TokenValidationParameters
+                {
+                    ClockSkew = TimeSpan.Zero, // default: 5 min
+                    RequireSignedTokens = true,
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+
+                    RequireExpirationTime = true,
+                    ValidateLifetime = true,
+
+                    ValidateAudience = true, //default : false
+                    ValidAudience = jwtSettings.Audience,
+
+                    ValidateIssuer = true, //default : false
+                    ValidIssuer = jwtSettings.Issuer,
+
+                    TokenDecryptionKey = new SymmetricSecurityKey(encryptionKey)
+                };
+
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = validationParameters;
+                options.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        var logger = context.HttpContext
+                            .RequestServices
+                            .GetRequiredService<ILoggerFactory>()
+                            .CreateLogger(nameof(JwtBearerEvents));
+
+                        logger.LogError("Authentication failed.", context.Exception);
+
+                        if (context.Exception != null)
+                            throw new Exception(
+                                "Authentication failed.");
+
+                        return Task.CompletedTask;
+                    },
+                    OnTokenValidated = async context =>
+                    {
+                        var signInManager = context.HttpContext.RequestServices
+                            .GetRequiredService<SignInManager<User>>();
+                        var userRepository = context.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
+
+                        var claimsIdentity = context.Principal.Identity as ClaimsIdentity;
+
+                        if (claimsIdentity?.Claims?.Any() != true)
+                            context.Fail("This token has no claims.");
+
+                        var securityStamp =
+                            claimsIdentity.FindFirstValue(new ClaimsIdentityOptions().SecurityStampClaimType);
+                        if (!securityStamp.HasValue())
+                            context.Fail("This token has no secuirty stamp");
+
+                        //Find user and token from database and perform your custom validation
+                        var userId = claimsIdentity.GetUserId<int>();
+                        var user = await userRepository.GetByIdAsync(context.HttpContext.RequestAborted, userId);
+
+
+                        var validatedUser = await signInManager.ValidateSecurityStampAsync(context.Principal);
+                        if (validatedUser == null)
+                            context.Fail("Token secuirty stamp is not valid.");
+
+                        if (!user.IsActive)
+                            context.Fail("User is not active.");
+
+                        await userRepository.UpdateLastLoginDateAsync(user, context.HttpContext.RequestAborted);
+                    },
+                    OnChallenge = context =>
+                    {
+                        var logger = context.HttpContext
+                            .RequestServices
+                            .GetRequiredService<ILoggerFactory>()
+                            .CreateLogger(nameof(JwtBearerEvents));
+
+                        logger.LogError("OnChallenge error", context.Error, context.ErrorDescription);
+
+                        if (context.AuthenticateFailure != null)
+                            throw new Exception("Authenticate failure.");
+
+                        throw new Exception("You are unauthorized to access this resource.");
+
+                        //return Task.CompletedTask;
+                    }
+                };
+            });
+        }
+    }
+}
