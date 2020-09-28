cc46991e5902551769a212b2230c283cd60ac723
amingolmahalle <a.golmahalleh@alibaba.ir>
2020-09-28 23:17:13 +0330

    Add Models Directory

---
+++ Web/Models/TokenRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+    public class TokenRequest
+    {
+        [Required]
+        public string GrantType { get; set; }
+        
+        public string Username { get; set; }
+        
+        public string Password { get; set; }
+        
+        public string RefreshToken { get; set; }
+        
+        public string Scope { get; set; }
+
+        public string ClientId { get; set; }
+        
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ Web/Models/UserDto.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Entities.User;
+
+namespace Web.Models
+{
+    public class UserDto : IValidatableObject
+    {
+        [Required] [StringLength(100)] public string UserName { get; set; }
+
+        [Required] [StringLength(100)] public string Email { get; set; }
+
+        [Required] [StringLength(500)] public string Password { get; set; }
+
+        [Required] [StringLength(100)] public string FullName { get; set; }
+
+        public int Age { get; set; }
+
+        public GenderType Gender { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Password.Equals("123456"))
+                yield return new ValidationResult("Password cannot be 123456", new[] {nameof(Password)});
+
+            if (Gender == GenderType.Male && Age > 30)
+                yield return new ValidationResult("Men more than 30 years are not valid",
+                    new[] {nameof(Gender), nameof(Age)});
+        }
+    }
+}
