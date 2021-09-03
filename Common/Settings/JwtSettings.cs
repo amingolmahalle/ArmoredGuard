ca67ec548a9b82f17559d16871e068c36d93402a
hamed <naeemaei@outlook.com>
2021-09-03 17:35:30 +0430

    Add properties to SetClaim method Fix bug of token expire time

--- Common/Settings/JwtSettings.cs
+++ Common/Settings/JwtSettings.cs
@@ -3,15 +3,15 @@ namespace Common.Settings
     public class JwtSettings
     {
         public string SecretKey { get; set; }
-        
+
         public string EncryptKey { get; set; }
-        
+
         public string Issuer { get; set; }
-        
+
         public string Audience { get; set; }
-        
+
         public int NotBeforeMinutes { get; set; }
-        
-        public int ExpirationTime { get; set; }
+
+        public int ExpirationMinutes { get; set; }
     }
 }
--- Services/Services/JwtService.cs
+++ Services/Services/JwtService.cs
@@ -39,7 +39,7 @@ namespace Services.Services
                 _createSecurityTokenDescriptor(signingCredentials, encryptingCredentials, claims);
 
             JwtSecurityToken jwtSecurityToken = new JwtSecurityTokenHandler().CreateJwtSecurityToken(descriptor);
-            
+
             return new AccessTokenDto(jwtSecurityToken);
         }
 
@@ -54,7 +54,7 @@ namespace Services.Services
                 Audience = _securitySettings.JwtSettings.Audience,
                 IssuedAt = DateTime.Now,
                 NotBefore = DateTime.Now.AddMinutes(_securitySettings.JwtSettings.NotBeforeMinutes),
-                Expires = DateTime.Now.AddHours(_securitySettings.JwtSettings.ExpirationTime),
+                Expires = DateTime.Now.AddMinutes(_securitySettings.JwtSettings.ExpirationMinutes),
                 SigningCredentials = signingCredentials,
                 EncryptingCredentials = encryptingCredentials,
                 Subject = new ClaimsIdentity(claims)
@@ -67,6 +67,9 @@ namespace Services.Services
             ICollection<Claim> claims = new Collection<Claim>();
             claims.AddUserId(claimsDto.UserId.ToString());
             claims.AddUserName(claimsDto.Username);
+            claims.AddFirstName(claimsDto?.FirstName);
+            claims.AddLastName(claimsDto?.LastName);
+            claims.AddMobileNumber(claimsDto.MobileNumber);
             claims.AddSecurityStamp(claimsDto.SecurityStamp);
             claims.AddRoles(claimsDto.Roles.ToArray());
 
